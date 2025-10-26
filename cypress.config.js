a5057c2346023ff13be5bd9aa3977983b9517dcd
RafaelRezende75 <rrezende75@gmail.com>
2025-10-26 00:48:00 -0300

    Generating logs on pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true,
+    reportPageTitle: 'Test Results - ServeRest API',
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
   e2e: {
     baseUrl: 'https://serverest.dev',
     specPattern: 'cypress/e2e/**/*.cy.js',
